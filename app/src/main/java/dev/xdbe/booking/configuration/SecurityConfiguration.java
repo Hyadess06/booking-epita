d05e38e0569a471b197caa138b67675eaa9a4bfa
Franceline Ngo <franceline.ngo@epita.fr>
2025-03-20 17:08:11 +0100

    feat: add login form

--- app/src/main/java/dev/xdbe/booking/configuration/SecurityConfiguration.java
+++ app/src/main/java/dev/xdbe/booking/configuration/SecurityConfiguration.java
@@ -1,23 +1,14 @@
 package dev.xdbe.booking.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
-import org.springframework.session.web.http.CookieSerializer;
-import org.springframework.session.web.http.DefaultCookieSerializer;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -29,16 +20,31 @@ public class SecurityConfiguration {
         return http
             .authorizeHttpRequests(auth -> auth
                 // Step 3: add authorization
+                .requestMatchers("/dashboard").hasRole("ADMIN")
                 .anyRequest().permitAll()
             )
             // Step 3: Add login form
-            .csrf((csrf) -> csrf
-                .ignoringRequestMatchers("/h2-console/*")
-            )
+            .formLogin(withDefaults())
+            .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/*"))
             .headers(headers -> headers.frameOptions().disable())
             .build();
     }
 
     // Step 3: add InMemoryUserDetailsManager
+    @Bean
+    public UserDetailsService users() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+            .username("admin")
+            .password("admin")
+            .roles("ADMIN")
+            .build();
 
+        UserDetails user = User.withDefaultPasswordEncoder()
+            .username("user")
+            .password("user")
+            .roles("USER")
+            .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
 }
